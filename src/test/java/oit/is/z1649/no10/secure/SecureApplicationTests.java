9fe4071b04bb444898c5b34021ba566275f00175
e1b19031 <amazki@icloud.com>
2021-10-12 14:04:36 +0900

    first commit

---
+++ src/test/java/oit/is/z1649/no10/secure/SecureApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.z1649.no10.secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
